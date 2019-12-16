5e0f77067c874113aa11eb41608f5f56192d2470
Clecio013 <clecioaljunior@gmail.com>
2019-12-16 15:09:01 -0300

    Aula 10

--- src/components/layout/LayoutNewExpense.vue
+++ src/components/layout/LayoutNewExpense.vue
@@ -5,25 +5,37 @@
       Novo gasto
     </button>
 
-    <div class="modal fade" :class="{ show: showModal }" :style="{ display: showModal ? 'block' : 'none' }">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Título do modal</h5>
-            <button type="button" @click="closeModal()" class="close" aria-label="Fechar">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <p>Lorem ipsum is amet</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" @click="closeModal()" class="btn btn-secondary">Fechar</button>
-            <button type="button" class="btn btn-primary">Salvar mudanças</button>
+    <form @submit.prevent="submit()">
+      <div class="modal fade" :class="{ show: showModal }" :style="{ display: showModal ? 'block' : 'none' }">
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Adicionar um novo gasto</h5>
+              <button type="button" @click="closeModal()" class="close" aria-label="Fechar">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <div class="row">
+                <div class="form-group col-8" required>
+                  <label for="description">Descrição</label>
+                  <input v-model="form.description" type="text" name="description" id="description" class="form-control">
+                </div>
+
+                <div class="form-group col-4" required>
+                  <label for="value">Valor ($RS)</label>
+                  <input v-model="form.value"  type="text" name="value" id="value" class="form-control">
+                </div>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" @click="closeModal()" class="btn btn-secondary">Fechar</button>
+              <button type="submit" class="btn btn-primary">Incluir novo gasto</button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </form>
 
     <div class="modal-backdrop fase show" :style="{ display: showModal ? 'block' : 'none' }"></div>
   </div>
@@ -34,13 +46,46 @@ export default {
   name: 'LayoutNewExpense',
   data () {
     return {
-      showModal: false
+      showModal: false,
+      form: {
+        description: '',
+        value: ''
+      }
     }
   },
   methods: {
+    submit () {
+      this.$root.$emit('Spinner::show')
+      const ref = this.$firebase.database().ref(window.uid)
+      const id = ref.push().key
+
+      const payload = {
+        id,
+        receipt: null,
+        value: this.form.value,
+        createdAt: new Date().getTime(),
+        description: this.form.description
+      }
+
+      ref.child(id).set(payload, err => {
+        this.$root.$emit('Spinner::hide')
+
+        if (err) {
+          console.log(err)
+        } else {
+          this.closeModal()
+        }
+      })
+    },
     closeModal () {
       this.showModal = false
     }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.modal {
+  color: var(--darker);
+}
+</style>
